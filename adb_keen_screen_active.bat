558cc3ca1ede2bc47f230aa048d7e38669da11e8
sunnydodti <sunnydodti@gmail.com>
2024-10-17 01:29:25 +0530

    script - keep screen active

---
+++ adb_keen_screen_active.bat
@@ -0,0 +1,10 @@
+@echo off
+:start
+adb shell input tap 1 1
+adb -s 192.168.137.195 shell input tap 1 1
+powershell -nop -c "& {sleep 550}"
+echo | set /p="."
+goto :start
+
+@REM adb should be connected
+@REM clicks on the screen at regular interval
