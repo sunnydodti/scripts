623ee1c45a8a317daefb203051db690a6fd8ef46
sunnydodti <sunnydodti@gmail.com>
2024-10-17 01:26:24 +0530

    script - start screen share

---
+++ adb_start_screen_share_tcpip.bat
@@ -0,0 +1,3 @@
+scrcpy --tcpip -m 720
+@REM scrcpy should be in path
+@REM adb should be connected over tcp
---
+++ adb_start_screen_share_usb.bat
@@ -0,0 +1,3 @@
+scrcpy --select-usb -m 720
+@REM scrcpy should be in path
+@REM adb should be connected over usb
